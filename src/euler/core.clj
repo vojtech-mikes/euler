f1b78a89a17812e86808ecb980c006b861980715
Vojtech Mikes <vojtechmikes98@gmail.com>
2023-03-03 21:02:57 +0100

    solved number 2

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -5,3 +5,9 @@
 
 (reduce + (filterv #(or (= (mod % 3) 0) (= (mod % 5) 0)) (range 1 1000)))
 
+;; Problem 2
+
+(defn fibseq [x s] (if (< (second x) 4000000) 
+                     (recur (list (second x) (reduce + x))
+                            (if (even? (reduce + x)) (+ s (reduce + x)) s)) s))
+
